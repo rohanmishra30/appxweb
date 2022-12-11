62023f8d6b88f47aad2b18a1ad81457aa2948bf0
rohanmishra30 <91248195+rohanmishra30@users.noreply.github.com>
2022-12-11 22:35:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+function showfeature() {
+
+    var class1 = document.getElementById("prev1")
+    var class2 = document.getElementById("checkbox1")
+    class1.classList.toggle("showing1");
+    class2.classList.toggle("showing1");
+
+};
+function showfeature2() {
+
+    var class3 = document.getElementById("prev2")
+    var class4 = document.getElementById("checkbox2")
+    class3.classList.toggle("showing2");
+    class4.classList.toggle("showing2");
+    
+
+
+};
+function showfeature3() {
+
+    var class5 = document.getElementById("prev3")
+    var class6 = document.getElementById("checkbox3")
+    class5.classList.toggle("showing3");
+    class6.classList.toggle("showing3");
+};
+function showfeature4() {
+    var class7 = document.getElementById("prev4")
+    var class8 = document.getElementById("checkbox4")
+    class7.classList.toggle("showing4");
+    class8.classList.toggle("showing4");
+};
+function showfeature5() {
+    var class9 = document.getElementById("prev5")
+    var class10 = document.getElementById("checkbox5")
+    class9.classList.toggle("showing5");
+    class10.classList.toggle("showing5");
+};
+function showfeature6() {
+    var class11 = document.getElementById("prev6")
+    var class12 = document.getElementById("checkbox6")
+    class11.classList.toggle("showing6");
+    class12.classList.toggle("showing6");
+};
+
+function selectui() {
+    var ui=document.getElementById("ui")
+    ui.style.border="2px solid #FF0000";
+    ui.style.height="29rem";
+    ui.style.margin="0rem 1rem 0rem 1rem";
+};
+function selectui2() {
+    var ui2=document.getElementById("ui2")
+    ui2.style.border="2px solid #FF0000";
+    ui2.style.height="29rem";
+    ui2.style.margin="0rem 1rem 0rem 1rem";
+};
+function selectui3() {
+    var ui3=document.getElementById("ui3")
+    ui3.style.border="2px solid #FF0000";
+    ui3.style.height="29rem";
+    ui3.style.margin="0rem 1rem 0rem 1rem";
+};
